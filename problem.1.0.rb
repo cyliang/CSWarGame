2fe16ae29f8c64c05ec8aed76849dbbdf9864f5a
Chih-Yung Liang <stu89424@gmail.com>
2014-09-17 18:12:36 +0800

    The module for the first problem.

---
+++ problem.1.0.rb
@@ -0,0 +1,70 @@
+require 'io/console'
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+	def initialize
+		super(
+			'Name' => 'Connect to server! easy start',
+			'Version' => '$Revision:$',
+			'Description' => 'Get the key immediately by this module.',
+			'Author' => ['Femi'],
+			'Liciense' => MSF_LICENSE,
+			'Reference' => []
+		)
+
+		register_options([
+			OptAddress.new('WHOST', [
+				true,
+				'the workstation host',
+				'wargame2.cs.nctu.edu.tw'
+			]),
+			OptAddress.new('THOST', [
+				true,
+				'the target host',
+				''
+			]),
+			OptPort.new('TPORT', [
+				true,
+				'the target port',
+				''
+			]),
+			OptString.new('ACCOUNT', [
+				false,
+				'the website account',
+				''
+			]),
+			OptString.new('PASSWORD', [
+				false,
+				'the website password',
+				''
+			])
+		], self.class)
+	end
+
+	def run
+		whost = datastore['WHOST']
+		thost = datastore['THOST']
+		tport = datastore['TPORT']
+
+		account = datastore['ACCOUNT']
+		password = datastore['PASSWORD']
+		if account.empty?
+			print "Please enter your account: "
+			account = gets
+		end
+		if password.empty?
+			print "Please enter your password: "
+			password = STDIN.noecho(&:gets)[0..-2]
+		end
+
+		puts "\nPlease wait for the result..."
+		Net::SSH.start(whost, account, :password => password) do |ssh|
+			ssh.exec!("{ echo '#{account}'; sleep 1; } | telnet #{thost} #{tport} | grep 'key: ' | cut -d ' ' -f 2") do |ch2, stream, data|
+				if stream == :stdout
+					puts "Your key is: \e[1;33m#{data}\e[m"
+				end
+			end
+		end
+	end
+end
+
